6cd1e0c1263d8f31e5a88716893c8c02ca09330c
Aurelian Spodarec <aurelianxspodarec@gmail.com>
2020-05-28 22:36:04 +0100

    Modified the product list and now showing variations

--- routes.php
+++ routes.php
@@ -132,11 +132,11 @@
 
 
 
-
-    function custom_product_list() {
+    // , $fr_products_per_page
+    function custom_product_list( $fr_page) {
         global $product;
 
-        $currentPage = 1;
+        $currentPage = $fr_page;
         $productsPerPage = 2;
         $totalPages = ceil($total / $limit);
 
@@ -224,7 +224,7 @@
         endforeach;
         endif;
 
-        return $productResults;
+        return $$fr_page;
     }
 
     
@@ -239,7 +239,7 @@
             'method' => 'GET',
             'callback' => 'products_by_slug_variation'
         ));
-        register_rest_route( 'wc/v3', '/custom-product-list', array(
+        register_rest_route( 'wc/v3', '/custom-product-list?page=(?P<fr_page>[a-zA-Z0-9-]+)', array(
             'method' => 'GET',
             'callback' => 'custom_product_list'
         ));
@@ -247,6 +247,54 @@
 
     
 
-    // function get_min_max_woocommerce() {
+    // filter[limit]=25&filter[offset]="+offset;
 
-    // }
+add_filter('woocommerce_rest_prepare_product_object', 'custom_change_product_response', 20, 3);
+add_filter('woocommerce_rest_prepare_product_variation_object', 'custom_change_product_response', 20, 3);
+
+function custom_change_product_response($response, $object, $request) {
+
+    $variations = $response->data['variations'];
+    $variations_res = [];
+    $variations_array = [];
+
+ 
+
+    if (!empty($variations) && is_array($variations)) {
+        foreach ($variations as $variation) {
+            $variation_id = $variation;
+            $variation = new WC_Product_Variation($variation_id);
+
+            $variations_res['id'] = $variation_id;
+            $variations_res['on_sale'] = $variation->is_on_sale();
+            $variations_res['regular_price'] = (float)$variation->get_regular_price();
+            $variations_res['sale_price'] = (float)$variation->get_sale_price();
+            $variations_res['sku'] = $variation->get_sku();
+            $variations_res['quantity'] = $variation->get_stock_quantity();
+            $variations_res['image'] = wp_get_attachment_image_src(get_post_thumbnail_id( $variation_id ), false );;
+
+            if ($variations_res['quantity'] == null) {
+                $variations_res['quantity'] = '';
+            }
+            
+            $variations_res['stock'] = $variation->get_stock_quantity();
+
+            $attributes = array();
+            // variation attributes
+            foreach ( $variation->get_variation_attributes() as $attribute_name => $attribute ) {
+                // taxonomy-based attributes are prefixed with `pa_`, otherwise simply `attribute_`
+                $attributes[] = array(
+                    'name'   => wc_attribute_label( str_replace( 'attribute_', '', $attribute_name ), $variation ),
+                    'slug'   => str_replace( 'attribute_', '', wc_attribute_taxonomy_slug( $attribute_name ) ),
+                    'option' => $attribute,
+                );
+            }
+
+            $variations_res['attributes'] = $attributes;
+            $variations_array[] = $variations_res;
+        }
+    }
+    $response->data['product_variations'] = $variations_array;
+
+    return $response;
+}
